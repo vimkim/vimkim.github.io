74a5944f1bc16cb1bf2c337bf7ea4a499292e7c6
Daehyun Kim <kimdhyungg@gmail.com>
2024-03-01 00:18:51 +0900

    bugfix still mobile not working

--- assets/js/slide-animation.js
+++ assets/js/slide-animation.js
@@ -1,41 +1,42 @@
-// Function to check if element is in view
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  console.log(rect);
-  return rect.top <= window.innerHeight + 100 && rect.bottom >= -100;
-}
+// // Function to check if element is in view
+// function isInViewport(element) {
+//   const rect = element.getBoundingClientRect();
+//   console.log(rect);
+//   console.log(window.innerHeight);
+//   return rect.top <= window.innerHeight && rect.bottom >= 0;
+// }
 
-// Function to run when scrolling
-function checkVisibility() {
-  // Select specific elements to check if they are in the viewport
-  document.querySelectorAll("body *").forEach((item) => {
-    if (isInViewport(item)) {
-      item.classList.add("is-visible");
-    } else {
-      item.classList.remove("is-visible"); // Remove class if element is out of view
-    }
-  });
-}
+// // Function to run when scrolling
+// function checkVisibility() {
+//   // Select specific elements to check if they are in the viewport
+//   document.querySelectorAll("body *").forEach((item) => {
+//     if (isInViewport(item)) {
+//       item.classList.add("is-visible");
+//     } else {
+//       item.classList.remove("is-visible"); // Remove class if element is out of view
+//     }
+//   });
+// }
 
-let checkVisibilityDebounced;
+// let checkVisibilityDebounced;
 
-function debounce(func, wait, immediate) {
-  return function () {
-    const context = this,
-      args = arguments;
-    clearTimeout(checkVisibilityDebounced);
-    checkVisibilityDebounced = setTimeout(function () {
-      checkVisibilityDebounced = null;
-      if (!immediate) func.apply(context, args);
-    }, wait);
-    if (immediate && !checkVisibilityDebounced) {
-      func.apply(context, args);
-    }
-  };
-}
+// function debounce(func, wait, immediate) {
+//   return function () {
+//     const context = this,
+//       args = arguments;
+//     clearTimeout(checkVisibilityDebounced);
+//     checkVisibilityDebounced = setTimeout(function () {
+//       checkVisibilityDebounced = null;
+//       if (!immediate) func.apply(context, args);
+//     }, wait);
+//     if (immediate && !checkVisibilityDebounced) {
+//       func.apply(context, args);
+//     }
+//   };
+// }
 
-// Throttle scroll event
-window.addEventListener("scroll", debounce(checkVisibility, 20));
+// // Throttle scroll event
+// window.addEventListener("scroll", debounce(checkVisibility, 20));
 
-// Initial check in case elements are in viewport on page load
-document.addEventListener("DOMContentLoaded", checkVisibility);
+// // Initial check in case elements are in viewport on page load
+// document.addEventListener("DOMContentLoaded", checkVisibility);
